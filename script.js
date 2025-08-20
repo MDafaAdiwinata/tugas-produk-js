df41994e76f032fb9a7dfa513de9d665ac8628df
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-08-20 19:44:45 +0700

    Add Array produk - JS

---
+++ script.js
@@ -0,0 +1,30 @@
+let produk = [
+  { 
+    nama: "Kaos Polos", 
+    harga: 50000, 
+    deskripsi: "Kaos nyaman dipakai.", 
+    kategori: "Fashion",
+    gambar: "https://via.placeholder.com/300x200?text=Kaos+Polos"
+  },
+  { 
+    nama: "Celana Jeans", 
+    harga: 120000, 
+    deskripsi: "Celana jeans pria.", 
+    kategori: "Fashion",
+    gambar: "https://via.placeholder.com/300x200?text=Celana+Jeans"
+  },
+  { 
+    nama: "Headphone", 
+    harga: 250000, 
+    deskripsi: "Suara jernih.", 
+    kategori: "Elektronik",
+    gambar: "https://via.placeholder.com/300x200?text=Headphone"
+  },
+  { 
+    nama: "Smartphone", 
+    harga: 2500000, 
+    deskripsi: "Kamera 48MP.", 
+    kategori: "Elektronik",
+    gambar: "https://via.placeholder.com/300x200?text=Smartphone"
+  }
+];
